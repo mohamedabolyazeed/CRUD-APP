dd74a1f7d8a65073d4d390ab33799a71b799886d
MohamedAboElYazeed <mohamedaboelyazeed920@gmail.com>
2025-07-05 15:27:20 +0300

    update red.me and DB

--- DB/connection.js
+++ DB/connection.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://mohamedaboelyazeed920:H1iPPlJG9GeOzcwN@cluster0.lbwsy.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect("mongodb+srv://");
     console.log("Connected to DB successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
